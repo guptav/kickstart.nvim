7ce51a96bd7596f70bf5a5d3ff9ef757696ffb72
Vaibhav Gupta <vaibhavgupta@box.com>
2025-08-30 01:09:47 -0700

    fix: updating the init.lua with more plugins

--- init.lua
+++ init.lua
@@ -195,7 +195,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
-require('keymaps')
+require 'keymaps'
 
 -- Highlight the trailing spaces
 vim.api.nvim_create_autocmd('ColorScheme', {
@@ -454,6 +454,7 @@ require('lazy').setup({
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
+      pcall(require('telescope').load_extension, 'noice')
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
@@ -1085,6 +1086,115 @@ require('lazy').setup({
     -- endfunction
     -- " }}}
   },
+  {
+    '3rd/image.nvim',
+    build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+    opts = {
+      processor = 'magick_cli',
+    },
+  },
+  -- lazy.nvim
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+    },
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    version = false, -- last release is way too old
+    event = 'InsertEnter',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+    },
+    -- Not all LSP servers add brackets when completing a function.
+    -- To better deal with this, LazyVim adds a custom option to cmp,
+    -- that you can configure. For example:
+    --
+    -- ```lua
+    -- opts = {
+    --   auto_brackets = { "python" }
+    -- }
+    -- ```
+    opts = function()
+      vim.api.nvim_set_hl(0, 'CmpGhostText', { link = 'Comment', default = true })
+      local cmp = require 'cmp'
+      local defaults = require 'cmp.config.default'()
+      local auto_select = true
+      return {
+        auto_brackets = {}, -- configure any filetype to auto add brackets
+        completion = {
+          completeopt = 'menu,menuone,noinsert' .. (auto_select and '' or ',noselect'),
+        },
+        preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
+        mapping = cmp.mapping.preset.insert {
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-n>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+          ['<C-p>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<CR>'] = LazyVim.cmp.confirm { select = auto_select },
+          ['<C-y>'] = LazyVim.cmp.confirm { select = true },
+          ['<S-CR>'] = LazyVim.cmp.confirm { behavior = cmp.ConfirmBehavior.Replace }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ['<C-CR>'] = function(fallback)
+            cmp.abort()
+            fallback()
+          end,
+          ['<tab>'] = function(fallback)
+            return LazyVim.cmp.map({ 'snippet_forward', 'ai_accept' }, fallback)()
+          end,
+        },
+        sources = cmp.config.sources({
+          { name = 'lazydev' },
+          { name = 'nvim_lsp' },
+          { name = 'path' },
+        }, {
+          { name = 'buffer' },
+        }),
+        formatting = {
+          format = function(entry, item)
+            local icons = LazyVim.config.icons.kinds
+            if icons[item.kind] then
+              item.kind = icons[item.kind] .. item.kind
+            end
+
+            local widths = {
+              abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
+              menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
+            }
+
+            for key, width in pairs(widths) do
+              if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
+                item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. '…'
+              end
+            end
+
+            return item
+          end,
+        },
+        experimental = {
+          -- only show ghost text when we show ai completions
+          ghost_text = vim.g.ai_cmp and {
+            hl_group = 'CmpGhostText',
+          } or false,
+        },
+        sorting = defaults.sorting,
+      }
+    end,
+    main = 'lazyvim.util.cmp',
+  },
+
   {
     'github/copilot.vim',
   },
